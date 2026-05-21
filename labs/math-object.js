a2bb2331c2cd65b20c56839cb0c4d7eda6036865
sloolana <sloolana2@gmail.com>
2026-05-21 16:10:40 +0300

    feat: did an assignment on Math object using javascript

---
+++ labs/math-object.js
@@ -0,0 +1,15 @@
+const largestNumber = Math.max(18, 7, 42, 3, 29);
+const difference = Math.abs(-17.5, 9.3);
+
+console.log(largestNumber);
+console.log(difference)
+
+const a = 3;
+const b = 4;
+const c = Math.hypot(a, b);
+
+console.log(c);
+
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+
+console.log(randomNumber);
