c94831aff1d4ce3da702f3a0238abfffb6689758
sloolana <sloolana2@gmail.com>
2026-06-15 11:54:44 +0300

    some exercises done

---
+++ functions/anonymous_functions/anonymous_function6.js
@@ -0,0 +1,11 @@
+const fruits = ["apple", "mango", "banana"];
+fruits.forEach(function(fruit) {
+	console.log("Fruit: ", fruit);
+});
+
+// map
+const numbers = [1, 2, 3, 4];
+
+const double = numbers.map(function(num) {
+	return num * 2 
+});
---
+++ functions/anonymous_functions/anonymous_function7.js
@@ -0,0 +1,5 @@
+const multipleNumbers = function(firstNumber, secondNumber) {
+	return firstNumber * secondNumber;
+};
+
+console.log(multipleNumbers(4, 5));
