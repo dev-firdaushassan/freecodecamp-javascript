50f6758f90f3f121bd78f7801e6854d69e85de0c
sloolana <sloolana2@gmail.com>
2026-06-22 14:59:06 +0300

    calculated sums

---
+++ Arrays/Arrays-intro.js
@@ -0,0 +1,18 @@
+let fruits = ["apple", "banana", "orange", "banana"];
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits.indexOf("banana", 0));
+
+
+console.log(fruits.indexOf("banana", 2));
+
+
+
+console.log(fruits.length);
+
+
+let colours = ["pink", "maroon", "purple", "white"];
+console.log(colours[2]);
+
+console.log(colours.indexOf("white", 2));
